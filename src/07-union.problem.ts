341782c50b0261cd2d896af8721676a2a20b82ff
nkmr <ryusei1597@gmail.com>
2024-07-22 16:10:01 +0900

    07-union

--- src/07-union.problem.ts
+++ src/07-union.problem.ts
@@ -5,7 +5,7 @@ import { z } from "zod";
 
 const Form = z.object({
   repoName: z.string(),
-  privacyLevel: z.string(),
+  privacyLevel: z.union([z.literal("private"), z.literal("public")]),
   //              ^ 🕵️‍♂️
 });
 
