4848d776c310832fa959b7ef0a2e57ca6697132e
nkmr <ryusei1597@gmail.com>
2024-07-22 18:09:16 +0900

    14-generic

--- src/14-generics.problem.ts
+++ src/14-generics.problem.ts
@@ -4,8 +4,10 @@ import { it } from "vitest";
 import { z } from "zod";
 import { Equal, Expect } from "./helpers/type-utils";
 
-const genericFetch = (url: string, schema: z.ZodSchema) => {
-  //                 ^ 🕵️‍♂️
+const genericFetch = <T extends z.ZodSchema>(
+  url: string,
+  schema: T
+): Promise<z.infer<T>> => {
   return fetch(url)
     .then((res) => res.json())
     .then((result) => schema.parse(result));
@@ -18,11 +20,11 @@ it("Should fetch from the Star Wars API", async () => {
     "https://www.totaltypescript.com/swapi/people/1.json",
     z.object({
       name: z.string(),
-    }),
+    })
   );
 
   type cases = [
     // Result should equal { name: string }, not any
-    Expect<Equal<typeof result, { name: string }>>,
+    Expect<Equal<typeof result, { name: string }>>
   ];
 });
